fdac25e977a75c8f7ca186c4d7c04fdc3e5e81dd
Richard Brown <fluke_fan@hotmail.com>
2019-02-14 13:14:25 +0000

    Added placeholder project for sample of generating DAO for Dapper.

---
+++ Samples/DapperDao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DapperDao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static Task MainAsync(string[] args)
+        {
+            Console.WriteLine($"Hello from DapperDao");
+            return Task.CompletedTask;
+        }
+    }
+}
