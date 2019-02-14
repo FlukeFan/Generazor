9ad13d66762459931eeabaae0988167c81ae5a9a
Richard Brown <fluke_fan@hotmail.com>
2019-02-14 14:01:25 +0000

    Added test to check DapperDao is working.

---
+++ Generazor.Tests/Samples/DapperDaoTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Generazor.Tests.Samples
+{
+    [TestFixture]
+    public class DapperDaoTests
+    {
+        [Test]
+        public void OutputDisplaysArtists()
+        {
+            var results = Exec.CmdInProjectFolder("dotnet", "run", "Samples/DapperDao");
+
+            results.Output.Count.Should().BeGreaterThan(10);
+        }
+    }
+}
