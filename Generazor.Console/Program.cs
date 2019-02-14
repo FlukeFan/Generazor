81f52f9b41acd87c76b62ddeff2a9f6be49046c1
Richard Brown <fluke_fan@hotmail.com>
2019-02-14 14:48:46 +0000

    Added Generazor.Console placeholder.

---
+++ Generazor.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generazor.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("here!");
+        }
+    }
+}
---
+++ Samples/DapperDao/DaoGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DapperDao
+{
+    public interface ISetupGeneration
+    {
+        IList<GenerateInfo> Setup(string[] args);
+    }
+
+    public class GenerateInfo
+    {
+    }
+
+    public class DaoGenerator : ISetupGeneration
+    {
+        public IList<GenerateInfo> Setup(string[] args)
+        {
+            throw new System.Exception("Got to here");
+        }
+    }
+}
